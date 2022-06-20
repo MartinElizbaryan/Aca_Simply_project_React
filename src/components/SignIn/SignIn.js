3955ca13e0119c9d0e3110a2351f752c906f7089
avetisyan66 <avetisyanani6@gmail.com>
2022-06-20 19:44:54 +0400

    before pull dev

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import { Box, Checkbox, FormControlLabel, Button, Link } from '@mui/material';
+import { useState, useEffect } from "react";
+import { Box, Checkbox, FormControlLabel, Button, Link, Typography, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import { InputField } from "../Shared/InputField/InputField";
 import { signIn } from "./utils";
 import { colors } from "../../constants/styles";
@@ -7,30 +7,74 @@ import useStyles from './styles';
 
 
 export default function SignIn() {
+    const [open, setOpen] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [auth, setAuth] = useState(false);
+    const [errMessage, setErrMessage] = useState("");
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
     const classes = useStyles();
-    console.log(process.env.URL)
-
     return (
-            <Box className={classes.totalBox}>
-                <Box className={classes.centeral}>
-                    <InputField account={true} placeholder={"Your email"} value={email} onChange={(e)=>setEmail(e.target.value)} />
-                    <InputField password={true} placeholder={"Your password"} value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                </Box>
-                <Box className={classes.finalBox}>
-                    <FormControlLabel control={<Checkbox />} label="Remember me" style={{ color: colors.dark }} />
-                    <Link href="#" underline="none">
-                        {'Forgot your password?'}
-                    </Link>
-                </Box>
-                <Box className={classes.centeral2}>
-                    <Button variant="contained" color="success" onClick={()=>signIn({email, password})}>
-                        Log in
-                    </Button>
-                </Box>
+        <Box className={classes.totalBox}>
+            <Box className={classes.centeral}>
+                <InputField account={true} placeholder={"Your email"} value={email} onChange={(e) => setEmail(e.target.value)} />
+                <InputField password={true} placeholder={"Your password"} value={password} onChange={(e) => setPassword(e.target.value)} />
+            </Box>
+            <Box className={classes.finalBox}>
+                <FormControlLabel control={<Checkbox />} label="Remember me" style={{ color: colors.dark }} />
+                <Link href="#" underline="none">
+                    {'Forgot your password?'}
+                </Link>
+            </Box>
+            <Box className={classes.centeral2}>
+                <Button variant="contained" color="success" onClick={async () => {
+                    try {
+                        const data = await signIn({ email, password })
+                        console.log(data.auth)
+                        //setAuth(true);
+                        setOpen(false)
+                    } catch (e) {
+                        //setAuth(false)
+                        setOpen(true)
+                        setErrMessage(e.response.data.details)
+                        console.log(e)
+                    }
+                }
+                }>
+                    Log in
+                </Button>
+            </Box>
+            {open && <Box>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title" style={{textAlign: "center"}}>
+                        {"Incorrect email or password"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description" style={{textAlign: "center"}}>
+                        Your {errMessage}. <br/>Please try again.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions style={{display: "flex", justifyContent: "center"}}>
+                        <Button onClick={handleClose} autoFocus>
+                            Okay
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Box>}
         </Box>
     );
 }
