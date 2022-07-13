9075fc37b999748d9f8c2f07989eabfa1711844d
Martin ELizbaryan <martin.elizbaryan@gmail.com>
2022-07-13 21:55:36 +0400

    notifications create post

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,5 +1,7 @@
 import io from "socket.io-client"
 
+console.log("oricessssss ", process.env.REACT_APP_SERVER_BASE_URL_WITHOUT_VERSION)
+
 export const socket = io.connect(process.env.REACT_APP_SERVER_BASE_URL_WITHOUT_VERSION)
 
 export default socket
