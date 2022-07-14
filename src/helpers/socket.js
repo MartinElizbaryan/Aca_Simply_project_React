36a2ea71312fa82a3fedf00f33440557b85d2896
Martin ELizbaryan <martin.elizbaryan@gmail.com>
2022-07-14 20:49:25 +0400

    changes in socket

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -2,6 +2,7 @@ import io from "socket.io-client"
 
 export const socket = io.connect(process.env.REACT_APP_SERVER_BASE_URL_WITHOUT_VERSION, {
   transports: ["websocket", "polling"],
+  withCredentials: true,
 })
 
 export default socket
