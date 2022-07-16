07d4e7d8f2c36db17db857840cb07691be74fad2
Anahit Sargysan <anahit.sargsyan169@gmail.com>
2022-07-17 03:42:13 +0400

    change ThemeSwitcher

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { store } from "../redux/store"
 import { deleteUserInfo } from "../redux/userSlice"
+import { cookies } from "../helpers/cookies"
 
 const api = axios.create({
   withCredentials: true, // for cookie
@@ -14,6 +15,14 @@ api.interceptors.request.use((config) => {
 
 api.interceptors.response.use(
   (response) => {
+    const accessToken = cookies.get("accessToken")
+    // console.log(document.cookie)
+    cookies.remove("accessToken")
+    // console.log(accessToken)
+    if (!accessToken) return response
+    localStorage.setItem("accessToken", accessToken)
+    // console.log(cookies.get("accessToken"))
+
     return response
   },
   (error) => {
