abcd1ed463d245da0ed6403687bec2eef0282852
Anahit Sargysan <anahit.sargsyan169@gmail.com>
2022-07-19 02:01:09 +0400

    change useLazyFetch hook

--- src/components/Contact/utils.js
+++
@@ -1,7 +0,0 @@
-import api from "../../api/api"
-
-export const sendMessage = async (body) => {
-  const res = await api.post("/contact/send", body)
-  console.log(res)
-  return res.data
-}
--- src/components/SignIn/utils.js
+++
@@ -1,7 +0,0 @@
-import api from "../../api/api"
-
-export const signIn = async (body) => {
-  const res = await api.post("/auth/sign-in", body)
-  if (res.data.auth) localStorage.setItem("accessToken", res.data.accessToken)
-  return res.data
-}
